8105f1384ddfdca39b8cf111c94d412658a24e77
Mooonika90 <107703203+Mooonika90@users.noreply.github.com>
2023-07-28 10:49:10 +0200

    Update ScrollToTop.js

--- src/components/ScrollToTop.js
+++ src/components/ScrollToTop.js
@@ -1,12 +1,6 @@
-import { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
 
 function ScrollToTop() {
-	const { pathname } = useLocation();
 
-	useEffect(() => {
-		window.scrollTo(0, 0);
-	}, [pathname]);
 
 	return (
 		<button className='scroll-top-btn' onClick={() => window.scrollTo(0, 0)}>
